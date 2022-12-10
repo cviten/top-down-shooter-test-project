9ca2e37312d29195e88634810831f88046350f22
cviten <vitn3@yandex.ru>
2022-12-10 21:54:15 +0100

    Fix of user-created bullets

--- Backend/GameLogic/GameWorld.cpp
+++ Backend/GameLogic/GameWorld.cpp
@@ -87,7 +87,7 @@ void GameWorld::setInputs(const Input::Inputs& inputs) {
 
 void GameWorld::createBullet(Point startPosition, Direction direction, SpeedType speed) {
     const IDType id = bulletID++;
-    bullets.insert(std::make_pair(id, Bullet{id,Shapes::Circle({startPosition}, {7}), direction, speed ,GameObjectType::Bullet}));
+    bullets.insert(std::make_pair(id, Bullet{id,Shapes::Circle({startPosition}, {7}), normalize(direction), speed ,GameObjectType::Bullet}));
 }
 
 void GameWorld::setPlayField(const Size& screenSize) {
