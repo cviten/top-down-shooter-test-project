f99af2260fa0dd219c55a7438366a56815785bed
cviten <vitn3@yandex.ru>
2022-12-16 04:40:23 +0100

    Added test for bullet collisions

--- Tests/gameTests.h
+++ Tests/gameTests.h
@@ -8,6 +8,7 @@
 #include "doctest.h"
 
 #include "Backend/GameLogic/GameObject.h"
+#include "Backend/GameLogic/GameWorld.h"
 
 TEST_SUITE("Game")
 {
@@ -19,6 +20,78 @@ TEST_SUITE("Game")
         CHECK_GT(rectBody.getCollisionBody().getShape().size.y, doctest::Approx(0));
         CHECK_GT(circleBody.getCollisionBody().getShape().size.x, doctest::Approx(0));
         CHECK_GT(circleBody.getCollisionBody().getShape().size.y, doctest::Approx(0));
+    }
+    TEST_CASE("Game world")
+    {
+        GameWorld gameWorld;
+        gameWorld.setPlayField({1000, 1000});
+
+        SUBCASE("Level loading")
+        {
+            Level level;
+
+            level.enemyPosition.emplace_back(620, 250);
+            level.enemyPosition.emplace_back(570, 570);
+            level.enemyPosition.emplace_back(220, 220);
+            level.enemyPosition.emplace_back(100, 340);
+
+            level.wallPosition.emplace_back(200,100);
+            level.wallPosition.emplace_back(200, 100);
+            level.wallPosition.emplace_back(110, 370);
+            level.wallPosition.emplace_back(240, 190);
+            level.wallPosition.emplace_back(360, 230);
+            level.wallPosition.emplace_back(700, 170);
+
+            level.bulletSpawns.push_back(Level::BulletSpawn({0, 500}, {1, 0}));
+
+            gameWorld.setLevel(level);
+
+            REQUIRE_EQ(gameWorld.getStatus().enemyCount, 4);
+            REQUIRE_EQ(gameWorld.getStatus().wallCount, 6);
+            REQUIRE_EQ(gameWorld.getStatus().bulletCount, 1);
+        }
+        SUBCASE("Bullet Collision") {
+            SUBCASE("Bullets and Enemies") {
+                Level level;
+
+                level.enemyPosition.emplace_back(620, 250);
+                level.enemyPosition.emplace_back(570, 570);
+                level.enemyPosition.emplace_back(220, 220);
+                level.enemyPosition.emplace_back(100, 340);
+
+                level.bulletSpawns.push_back(Level::BulletSpawn({620, 250}, {1, 0}));
+                level.bulletSpawns.push_back(Level::BulletSpawn({570, 570}, {1, 0}));
+                level.bulletSpawns.push_back(Level::BulletSpawn({220, 220}, {1, 0}));
+                level.bulletSpawns.push_back(Level::BulletSpawn({100, 340}, {1, 0}));
+
+                gameWorld.setLevel(level);
+                gameWorld.process(0);
+
+                CHECK_EQ(gameWorld.getStatus().enemyCount, 0);
+                CHECK_EQ(gameWorld.getStatus().bulletCount, 4);
+            }
+
+            SUBCASE("Bullets and Walls") {
+                Level level;
+
+                level.wallPosition.emplace_back(620, 250);
+                level.wallPosition.emplace_back(570, 570);
+                level.wallPosition.emplace_back(220, 220);
+                level.wallPosition.emplace_back(100, 340);
+
+                level.bulletSpawns.push_back(Level::BulletSpawn({620, 250}, {1, 0}));
+                level.bulletSpawns.push_back(Level::BulletSpawn({570, 570}, {1, 0}));
+                level.bulletSpawns.push_back(Level::BulletSpawn({220, 220}, {1, 0}));
+                level.bulletSpawns.push_back(Level::BulletSpawn({100, 340}, {1, 0}));
+
+                gameWorld.setLevel(level);
+                gameWorld.process(0.0);
+
+                CHECK_EQ(gameWorld.getStatus().bulletCount, 0);
+                CHECK_EQ(gameWorld.getStatus().wallCount, 4);
+            }
+        }
+
     }
 }
 
